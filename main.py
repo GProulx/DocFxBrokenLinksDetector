ac93785173c2a92d59a045d2ae6dd29d77dcc1ca
Ghislain Proulx <Ghislain.Proulx@ia.ca>
2022-01-04 16:33:03 -0500

    Another iteration

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python
 
-import sys
+from bs4 import BeautifulSoup
+from queue import Queue
+from urllib.parse import urljoin
 import getopt
 import requests
+import sys
+import socket
+from requests.models import HTTPError
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+pages_queue = Queue(maxsize=0)
+pages_history = []
 
 
-def get_page(url):
-    page = requests.get(url, verify=False)
-    # for header in page.headers:
-    #     print(header, ":", page.headers[header])
-
-    # # for header in page.headers:
-    # #     print(header, ":", page.headers[header])
-
-    # print("Page.History: ", len(page.history))
-
-    # print("Page.links: ", page.links)
-
-    # print("Page.next: ", page.next)
-
-    # print(page.request)
-    return page
+def request_page(url):
+    try:
+        page = requests.get(url, verify=False)
+        return page
+    except socket.gaierror:
+        return None
 
 
 def get_page_content(page_content):
@@ -43,40 +40,95 @@ def is_valid_url(url):
         r'(?::\d+)?'  # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-    return re.match(regex, url) is not None
+    try:
+        return re.match(regex, url) is not None
+    except TypeError:
+        print("ERROR - Problem validating url", url)
+        raise
+
+
+def is_external_link(url, link):
+    # this should be improved...
+    return not link.startswith(url)
+
+
+def is_link_available(link):
+    try:
+        response = request_page(link)
+        if response == None:
+            return False
+        else:
+            return response.ok
+    except:
+        return False
 
 
 def process_page_content(url, links):
-    # urljoin('/media/path/', 'js/foo.js')
-
-    print("process_page_content")
-
     for link in links:
+
+        if link.get('href') == None:
+            continue
+
         if is_valid_url(link.get('href')):
-            print("VALID:", link.get('href'))
+            link = link.get('href')
+            if (is_external_link(url, link)):
+                is_valid = is_link_available(link)
+                print("EXTERNAL LINK:", link, is_valid)
+            else:
+                print("INTERNAL FULL LINK:", link)
         else:
-            print(urljoin(url, link.get('href')))
+            full_url = urljoin(url, link.get('href'))
+            print(full_url)
+            if is_link_available(full_url):
+                # print()
+                pages_queue.put(full_url)
+            else:
+                print("Page", url, "has a broken link pointing to",
+                      link.get('href'))
 
 
-def get_root_page(url):
-    root_page=get_page(url)
+def get_page_hash(page_content):
+    import hashlib
+    hash_object = hashlib.md5(page_content)
+    hash = hash_object.hexdigest()
+    # print(hash)
+    return (hash)
 
-    page_content=get_page_content(root_page.content)
 
-    process_page_content(url, page_content)
+def process_page_queue(root_url):
+    url = pages_queue.get()
+
+    print("Processing page:", url)
+
+    response = request_page(url)
+
+    page_content = get_page_content(response.content)
+
+    page_hash = get_page_hash(response.content)
+
+    if page_hash not in pages_history:
+        pages_history.append(page_hash)
+        process_page_content(url, page_content)
 
 
 def main(argv):
-    opts, args=getopt.getopt(argv, "u:", ["url="])
+    url = None
+    opts, args = getopt.getopt(argv, "u:", ["url=", "url2="])
     for opt, arg in opts:
         if opt == '-h':
             print('main.py --url <rootURL>')
             sys.exit()
         elif opt in ("-u", "--url"):
-            url=arg
+            url = arg
 
     if(url):
-        get_root_page(url)
+        root_url = url
+        pages_queue.put(root_url)
+        # pages_queue.put(urljoin(root_url, "/fr/toc.html"))
+        # pages_queue.put(urljoin(root_url, "/en/toc.html"))
+        while (not pages_queue.empty()):
+            process_page_queue(root_url)
+        print("Process completed!")
 
 
 if __name__ == "__main__":
